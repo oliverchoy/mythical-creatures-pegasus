c3a0f36ea3ed7b076daf64307bd362a51bed9d8c
Oliver Choy <oliver.choy@slalom.com>
2020-09-29 01:28:05 -0700

    Pegaus B

--- hello_lambda.py
+++ hello_lambda.py
@@ -3,7 +3,7 @@ import requests
 def lambda_handler(json_input, context):
     print(json_input)
     print(context)
-    print("Hello Pegasus A")
+    print("Hello Pegasus B")
 
 if __name__ == '__main__':
     lambda_handler({}, None)
